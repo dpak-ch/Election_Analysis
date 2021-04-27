21808a9c4ed91c357b40a00a7b71c085da56603f
Dpak-Ch <dpak.ch@hotmail.com>
2021-04-27 10:40:55 -0500

    Updated code to print election results

--- PyPoll.py
+++ PyPoll.py
@@ -16,6 +16,9 @@ total_votes = 0
 # Candidate list
 candidate_options = []
 
+# Candidate votes
+candidates_votes = {}
+
 # Using the with statement open the file as a text file.
 with open(file_to_load) as election_data:
     # Read the file object with the reader function.
@@ -29,8 +32,34 @@ with open(file_to_load) as election_data:
         total_votes +=1
 
         candidate_name = row[2]
-
+        
         if candidate_name not in candidate_options:
+            # Add candidate name to list
             candidate_options.append(candidate_name)
+            # initialize candidate vote count
+            candidates_votes[candidate_name] = 0
 
-print(candidate_options)
+        # Count candidate votes
+        candidates_votes[candidate_name] += 1
+
+winning_count = 0
+winning_pct = 0
+winning_candidate = ""
+
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidates_votes:
+    # 2. Retrieve vote count of a candidate.
+    votes = candidates_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
+    
+    vote_percentage = float(votes) / float(total_votes) * 100
+    
+    if (vote_percentage>winning_pct):
+        winning_pct = vote_percentage
+        winning_candidate = candidate_name
+        winning_count = votes
+            
+    # 4. Print the candidate name and percentage of votes.
+    print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+print(f"{winning_candidate} is the winner. She got {winning_count} votes which is ~{winning_pct:.1f}% of total votes")
